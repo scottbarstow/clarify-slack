461934ab139699c462612487250b410ff1a4dbeb
Vladimir Kalinkin <vova.kalinkin@gmail.com>
2015-09-01 10:14:52 +0300

    Improved mailer logging.

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -23,7 +23,9 @@ function Mailer() {
       html: content.html,
       text: content.text
     }, function(err){
-      console.log(err);
+      if (err) {
+        console.log(err);
+      }
     });
   }
 
